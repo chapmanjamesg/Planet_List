1511b548360b69953f7405af1991031227052f94
James Chapman <chapmanjamesg@gmail.com>
2020-01-13 15:01:46 -0600

    finished the exercise

--- planets.py
+++ planets.py
@@ -1,1 +1,14 @@
 planet_list = ["Mercury", "Mars"]
+
+planet_list.append("Jupiter")
+planet_list.append("Saturn")
+
+planet_list.extend(["Neptune", "Uranus"])
+
+planet_list.insert(2, "Venus")
+planet_list.insert(3, "Earth")
+
+planet_list.append("Pluto")
+
+del planet_list[8]
+print(planet_list)
